a65a326524e1fe5a69a93e4d4e7c3554b5e6974a
Vincent MAGNIEN <vincentmagnienpro@gmail.com>
2024-05-29 01:50:22 +0200

    fix: indestructible box on projectiles

--- Assets/Scripts/PickUps/OpenChest.cs
+++ Assets/Scripts/PickUps/OpenChest.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class OpenChest : MonoBehaviour
 {
     [SerializeField] private GameObject openedSprite;
+    [SerializeField] private GameObject teleportActivate;
+    [SerializeField] private AudioClip openSound = null;
+
+    private bool isOpened = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<DamageSource>())
+        if (!isOpened && other.gameObject.GetComponent<DamageSource>())
         {
             GetComponent<PickUpSpawner>().DropTeleportStone();
             transform.GetChild(0).gameObject.SetActive(false);
+            teleportActivate.SetActive(true);
             openedSprite.SetActive(true);
+            GetComponent<AudioSource>().PlayOneShot(openSound);
+            isOpened = true;
         }
     }
 }
